018449a6e09c7b7adf20087839e0f4891df9176f
SanaBrian <brianscope27@gmail.com>
2024-07-22 02:31:56 +0000

    Correcciones sugeridas y uso de máquina de estados

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include "mbed.h"
 
-#ifndef ON
-#define ON  1
+#ifndef ENCENDIDO
+#define ENCENDIDO  1
 #endif
 
-#ifndef OFF
-#define OFF  !(ON)
+#ifndef APAGADO
+#define APAGADO  !(ENCENDIDO)
 #endif
+
+#typedef enum{
+    CALENTANDO,
+    NORMAL,
+    ENFRIANDO
+} processState_t;
 
 #define delay(ms)   thread_sleep_for(ms)
 #define REGISTRY_TIME_INCREMENT   10
@@ -31,35 +37,46 @@ float lm35ReadingsArray[NUMBER_OF_AVG_SAMPLES];
 float lm35ReadingsSum = 0.0;
 float lm35ReadingsAverage = 0.0;
 float lm35TempC = 0.0;
+processState_t processState = NORMAL;
 
 //--------- Declaración de Funciones prototipos ----------
 void initOuputs();
 void checkOverHeating();
 void checkOverCooling();
 void checkRegTime();
-int tempConverter(float ratio);                                 //El ratio es la señal analógica convertida a digital entre 0.0 y 1.0
-void samplerAverager();    
+float tempConverter(float ratio);
+void initSamplerAverager();                                 //El ratio es la señal analógica convertida a digital entre 0.0 y 1.0
+void samplerAverager();
+void stateUpdate();     
 
 
 // main() runs in its own thread in the OS
 int main()
 {
     initOuputs();
+    initSamplerAverager();
     while (true) {
         samplerAverager();
         checkOverHeating();
         checkOverCooling();
+        stateUpdate();
         checkRegTime();
     }
 }
 
 void initOuputs(){
-    maxTempLED = OFF;
-    minTempLED = OFF;
-    normalTempLED = ON;
+    maxTempLED = APAGADO;
+    minTempLED = APAGADO;
+    normalTempLED = ENCENDIDO;
 }
 
-int tempConverter(float v){                                    //Convierte señal eléctrica a ºC y actualiza el valor de temperatura
+void initSamplerAverager(){
+    for (int i = 0; i <= NUMBER_OF_AVG_SAMPLES; i++){
+        lm35ReadingsArray[i] = tempSensor.read();
+    }
+}
+
+float tempConverter(float v){                                    //Convierte señal eléctrica a ºC y actualiza el valor de temperatura
     float tempC = v * 3.3/0.01;
     return tempC;
 }
@@ -82,33 +99,18 @@ void samplerAverager(){
 
 
 void checkOverHeating(){                                        //Chequea si la temperatura está por arriba del limite
-    if(lm35TempC >= maxTempValue){
-        maxTempLED = ON;                                        //Activa un ventilador (representado por un LED)
-        normalTempLED = OFF;
+    if(lm35TempC > maxTempValue){
+        processState = ENFRIANDO;
         uartUsb.write("Alcanzó la máxima temperatura\r\n", 30); 
-        while (lm35TempC > avgTempValue){
-            maxTempLED = ON;
-            samplerAverager();
-        }
-        maxTempLED = OFF;                                       //Apaga el ventilador
-        normalTempLED = ON;
-        uartUsb.write("Temperatura normalizada\r\n", 24);    
+            
     }
 }
 
 
 void checkOverCooling(){                                        //Chequea si la temperatura está por abajo del limite
-    if(lm35TempC <= minTempValue){
-        minTempLED = ON;                                        //Activa una resistencia calefactora (representado por un LED)
-        normalTempLED = OFF;
+    if(lm35TempC < minTempValue){
+        processState = CALENTANDO;
         uartUsb.write("Alcanzó la mínima temperatura\r\n", 30);
-        while (lm35TempC <= avgTempValue){
-            minTempLED = ON;
-            samplerAverager();   
-        }
-        minTempLED = OFF;                                       //Apaga la resistencia calefactora
-        normalTempLED = ON;
-        uartUsb.write("Temperatura normalizada\r\n", 24);
     }
 }
 
@@ -124,7 +126,7 @@ void uartTask(){                                                    //Envia un r
 void checkRegTime(){                                            //Chequea el tiempo entre envíos de registros
     accum_time_reg = accum_time_reg + REGISTRY_TIME_INCREMENT;
     delay(REGISTRY_TIME_INCREMENT);
-    if(accum_time_reg == 1200000){                              //Tiempo de 20 min entre registros
+    if(accum_time_reg == 500){                              //Tiempo de 5 seg entre registros
         uartTask();                                             //Envía registro de temperatura 
         accum_time_reg = 0;
     }
